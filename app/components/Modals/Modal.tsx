85e57d42de2bf1cd816d2e27b655834515a910cb
samyusuf101 <samyusuf101@gmail.com>
2023-04-24 14:43:24 -0700

    updated the modal component

--- app/components/Modals/Modal.tsx
+++ app/components/Modals/Modal.tsx
@@ -64,7 +64,7 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <div
       className="justify-center items-center flex 
-  overflow-x-hidden overflow-y-auto fixed inset-0
+  overflow-x-hidden overflow-y-auto px-4 fixed inset-0
   z-50 outline-none focus:outline-none bg-neutral-800/70"
     >
       <div
@@ -79,10 +79,15 @@ const Modal: React.FC<ModalProps> = ({
         `}
         >
           <div
-            className="translate h-full lg:h-auto md:h-auto border-0
+            className="translate top-80 mt-10 md:mt-0 md:top-0 lg:h-auto md:h-auto border-0
             rounded-lg shadow-lg relative flex flex-col w-full
             bg-white outline-none focus:outline-none"
-          ></div>
+          >
+            <div
+              className="flex items-center p-6 rounded-t justify-center
+            relative border-b-[1px]"
+            ></div>
+          </div>
         </div>
       </div>
     </div>
