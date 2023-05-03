9d634bd5b7e38b969199016ba9527b5b9090e4f5
SamYusuf101 <samyusuf101@gomail.com>
2023-05-03 03:21:50 -0700

    test

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -8,7 +8,7 @@ const Avatar = () => {
       className="rounded-full"
       height="30"
       width="30"
-      alt="Avatar"
+      alt="Avataar"
       src="/images/placeholder.jpg"
     />
   );
